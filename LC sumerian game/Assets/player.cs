d4712a975232a2756282960b7ee8f8ee53fe577c
Hayden <markhamonca@codeninjas.com>
2024-08-25 13:48:37 -0400

    22

---
+++ LC sumerian game/Assets/player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class player : MonoBehaviour
+{
+    public string playername;
+    private ulong highestpop;
+    private GameplayManager gamemanager;
+
+    public TMP_Text nametext;
+    public TMP_Text yeartext;
+    public TMP_Text highestpoptext;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
