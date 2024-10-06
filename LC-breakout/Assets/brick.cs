be032ecdf87972d291a36aee8138fdfe25ce1fb0
Hayden <markhamonca@codeninjas.com>
2024-10-06 13:14:07 -0400

    Update brick.cs

--- LC-breakout/Assets/brick.cs
+++ LC-breakout/Assets/brick.cs
@@ -5,4 +5,15 @@ using UnityEngine;
 public class brick : MonoBehaviour
 {
     public GameObject nextBrick;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("brick"))
+        {
+            GameObject nextBrick = collision.gameObject.GetComponent<brick>().nextBrick;
+            if (nextBrick != null)
+            {
+                Instantiate(nextBrick, collision.gameObject.transform.position, Quaternion.identity);
+            }
+        }
+    }
 }
