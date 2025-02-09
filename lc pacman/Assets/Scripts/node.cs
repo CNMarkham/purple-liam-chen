82969aff5e50f9f832ad6d978dab71848fb7ecb0
To <markhamonca@codeninjas.com>
2025-02-09 13:58:18 -0500

    step 75 node script

---
+++ lc pacman/Assets/Scripts/node.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nodescript : MonoBehaviour
+{
+    public LayerMask obstacleLayer;
+    public List<Vector2> availableDriections;
+
+    void Start()
+    {
+        availableDriections = new List<Vector2>();
+
+        checkAvailableDirection(Vector2.up);
+        checkAvailableDirection(Vector2.down);
+        checkAvailableDirection(Vector2.left);
+        checkAvailableDirection(Vector2.right);
+    }
+
+    private void checkAvailableDirection(Vector2 newDirection)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0f, newDirection, 1.5f, obstacleLayer);
+        if (hit.collider == null)
+        {
+            availableDriections.Add(newDirection);
+        }
+    }
+}
