31846530880e6534c0dd5b2b0bf0cae3de907673
To <markhamonca@codeninjas.com>
2025-02-23 13:57:45 -0500

    step 83 ghost script, scattering

---
+++ lc pacman/Assets/Scripts/ghost.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghost : movement
+{
+    public GameObject body;
+    public GameObject eyes;
+    public GameObject blue;
+    public GameObject white;
+    public bool atHome;
+    public float homeDuration;
+    private bool frightned;
+
+    private void Awake()
+    {
+        // set the original state of the ghost
+    }
+
+    protected override void childUpdate()
+    {
+        // umm... nothing!
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // see if ghost hits pacman or home wall
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // check is ghost hits a node, change directions
+        nodescript node = collision.GetComponent<nodescript>();
+
+        if (node != null)
+        {
+            int index = Random.Range(0, node.availableDriections.Count);
+
+            setDirection(node.availableDriections[index]);
+        }
+    }
+
+    private void leaveHome()
+    {
+        // switch from home mode to scatter mode
+    }
+
+    public void fighten()
+    {
+        // set ghost to frightened if not at home
+    }
+
+    private void flash()
+    {
+        // make ghost flash white
+    }
+
+    private void Reset()
+    {
+        // turns ghost back to normal
+    }
+}
