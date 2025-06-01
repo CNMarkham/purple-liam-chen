258dd8410ccc410af113305423db56beaa321621
Hayden <markhamonca@codeninjas.com>
2025-06-01 13:51:39 -0400

    step 81 enemy movement

---
+++ lc mario/Assets/scripts/camera follow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        if (target.position.x > transform.position.x)
+        {
+            transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
