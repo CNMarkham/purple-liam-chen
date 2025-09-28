9a8e9f1b18bb264dc5faaa4d88996007af746a2c
Pookle444 <markhamonca@codeninjas.com>
2025-09-28 13:50:55 -0400

    step 17 locking movement

---
+++ lc tetris/Assets/Scenes/scripts/Tetromino.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour
+{
+    private float previousTime;
+    public float fallTime;
+    public int width = 10;
+    public int height = 20;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + Vector3.left;
+            if (!ValidMove())
+            {
+                transform.position = transform.position - Vector3.left;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + Vector3.right;
+            if (!ValidMove())
+            {
+                transform.position = transform.position - Vector3.right;
+            }
+        }
+
+        float tempTime = fallTime;
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tempTime = tempTime / 10;
+        }
+
+        if ((Time.time - previousTime) >= tempTime)
+        {
+            transform.position = transform.position + Vector3.down;
+            if (!ValidMove())
+            {
+                transform.position = transform.position - Vector3.down;
+            }
+            previousTime = Time.time;
+        }
+
+    }
+
+    public bool ValidMove()
+    {
+        foreach (Transform child in transform)
+        {
+            int x = Mathf.RoundToInt(child.transform.position.x);
+            int y = Mathf.RoundToInt(child.transform.position.y);
+            
+            if (x <= 0 || y <= 0 || x >= width || y >= height)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
