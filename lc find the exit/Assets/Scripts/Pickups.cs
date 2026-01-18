5bc19d9d2640abb3d52af2db1091bdc27a758973
quokkadude <markhamonca@codeninjas.com>
2026-01-18 13:22:17 -0500

    ninja run (find the exit?!) PY done

---
+++ lc find the exit/Assets/Scripts/Pickups.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour
+{
+    public ParticleSystem Pickup;
+    public GameObject Door;
+
+    void Start()
+    {
+        Pickup.Stop();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("coin"))
+        {
+            Debug.Log("trigger");
+            Destroy(other.gameObject);
+            Destroy(Door.gameObject);
+
+            Pickup.Play();
+        }
+    }
+}
