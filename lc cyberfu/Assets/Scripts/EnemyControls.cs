202de637f485398302df921afd3f2ea3ec9b937c
quokkadude <markhamonca@codeninjas.com>
2026-02-08 14:08:20 -0500

    cyberfu 22

---
+++ lc cyberfu/Assets/Scripts/EnemyControls.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControls : MonoBehaviour
+{
+    public float speed;
+    public float attackingSpeed;
+
+    private Animator animatorEnemy;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
