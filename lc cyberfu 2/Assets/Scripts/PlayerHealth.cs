6980d6a6d9cf76d37a3a4c20a3fa6f6bf2db6e45
Pookle444 <132835392+CNMarkham@users.noreply.github.com>
2026-03-18 17:30:48 -0400

    cyberfu 2 PY done

---
+++ lc cyberfu 2/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxPlayerHealth = 20;
+    public int currentPlayerHealth;
+    public int enemyDamage = 1;
+
+    public PlayerExplosionParticles particles;
+
+    private Animator playerAnimator;
+   
+    void Start()
+    {
+        currentPlayerHealth = maxPlayerHealth;
+        playerAnimator = GetComponent<Animator>();
+        particles = GetComponent<PlayerExplosionParticles>();
+    }
+    
+    public void HurtPlayer()
+    {
+        currentPlayerHealth -= enemyDamage;
+        playerAnimator.SetTrigger("Hit");
+
+        if (currentPlayerHealth <= 0)
+        {
+            particles.Explode();
+            Invoke("ReloadScene", 2);
+        }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene("CyberFu");
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HitCollider"))
+        {
+            HurtPlayer();
+        }
+    }
+}
