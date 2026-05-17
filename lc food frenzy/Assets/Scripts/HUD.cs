5e9c08ab8284421e8f24ec10fb0b689eae91b829
.(period) <markhamonca@codeninjas.com>
2026-05-17 13:54:50 -0400

    food frenzy 32

---
+++ lc food frenzy/Assets/Scripts/HUD.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HUD : MonoBehaviour
+{
+    public Level level;
+    public TMP_Text remainingText;
+    public TMP_Text remainingLabel;
+    public TMP_Text targetText;
+    public TMP_Text targetLabel;
+    public TMP_Text scoreText;
+    public Image starsImage;
+
+    public Sprite[] stars;
+
+    private int starIndex;
+    private bool isGameOver;
+
+    public static HUD instance;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ lc food frenzy/Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class LevelTimer : Level
+{
+    public int timeInSeconds;
+    public int targetScore;
+
+    private float timer;
+    private bool timeOut = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        type = LevelType.TIMER;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!timeOut)
+        {
+            timer += Time.deltaTime;
+
+            if (timeInSeconds - timer <= 0)
+            {
+                if (currentScore >= targetScore)
+                {
+                    GameWin();
+                }
+                else
+                {
+                    GameLose();
+                }
+
+                timeOut = true;
+            }
+        }
+    }
+
+    public string TimerString()
+    {
+        return string.Format("{0}:{1:00}", (int)Mathf.Max((timeInSeconds - timer) / 60), (int)Mathf.Max((timeInSeconds - timer) % 60));
+    }
+}
